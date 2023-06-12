1330b4aa189cc98a1e50f44000cd12ae2352d507
Easyvac16 <gdima9051@gmail.com>
2023-06-12 17:52:24 +0300

    Added record Answer

--- Exam_project/Record_answer.cs
+++ Exam_project/Record_answer.cs
@@ -5,9 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exam_project
-{
-    internal class Tasks
+{ 
+    record Answer
     {
-        
+        public string Text { get; set; }
+        public bool IsCorrect { get; set; }
+
+        public static void PrintAnswers(List<Answer> answers)
+        {
+            for (int i = 0; i < answers.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {answers[i].Text}");
+            }
+        }
+
+        public bool Cont(Answer answer) => (this.Text == answer.Text && this.IsCorrect == answer.IsCorrect);
+
     }
 }
