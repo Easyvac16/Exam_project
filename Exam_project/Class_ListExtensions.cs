0d02d480ce622d92603462734a3aa3a4667bbfa4
Easyvac16 <gdima9051@gmail.com>
2023-06-12 18:01:47 +0300

    Added class ListExtensions

--- Exam_project/Class_ListExtensions.cs
+++ Exam_project/Class_ListExtensions.cs
@@ -6,8 +6,22 @@ using System.Threading.Tasks;
 
 namespace Exam_project
 {
-    internal class Tasks
+    static class ListExtensions
     {
-        
+        private static Random random = new Random();
+
+        public static List<Answer> RandomSwap(List<Answer> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                var value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+            return list;
+        }
     }
 }
